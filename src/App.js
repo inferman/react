e3714f1cda9a8f9f57dfe595f359e8fbfa6f027e
inferman <infernal.manowar@gmail.com>
2019-08-20 17:47:37 +0300

    added hook

--- src/App.js
+++ src/App.js
@@ -1,50 +1,53 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: "Neo", spec: "choosen one" },
       { name: "Morfius", spec: "recruiter", yearOfB: "1964" },
       { name: "Trinity", spec: "white rabbit" }
     ]
-  };
-  switchNameHandler = _ => {
-    //console.log(event);
-    this.setState({
+  });
+
+  const [otherState, setOtherState] = useState("simple string");
+
+  const switchNameHandler = _ => {
+    setPersonsState({
       persons: [
         { name: "Neo", spec: "choosen one" },
         { name: "Morfius", spec: "recruiter", yearOfB: "1964" },
         { name: "Trinity", spec: "killer" }
       ]
     });
-    this.state.persons[2].name = "Some name";
+    personsState.persons[2].name = "Some name";
   };
-  render() {
-    return (
-      <div className="App">
-        <h1 className="testing classnames">test headline</h1>
-        <p>first component</p>
-        <button type="button" onClick={this.switchNameHandler}>
-          Switch Name
-        </button>
-        <Person
-          name={this.state.persons[1].name}
-          spec={this.state.persons[1].spec}
-          nums={this.state.persons[1].yearOfB}
-        />
-        <Person name="Neo" spec="choosen one" nums="1971">
-          Some inner text
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          spec={this.state.persons[2].spec}
-          nums="1983"
-        />
-      </div>
-    );
-  }
-}
 
-export default App;
+  console.log(personsState, otherState);
+
+  return (
+    <div className="App">
+      <h1 className="testing classnames">test headline</h1>
+      <p>first component</p>
+      <button type="button" onClick={switchNameHandler}>
+        Switch Name
+      </button>
+      <Person
+        name={personsState.persons[1].name}
+        spec={personsState.persons[1].spec}
+        nums={personsState.persons[1].yearOfB}
+      />
+      <Person name="Neo" spec="choosen one" nums="1971">
+        Some inner text
+      </Person>
+      <Person
+        name={personsState.persons[2].name}
+        spec={personsState.persons[2].spec}
+        nums="1983"
+      />
+    </div>
+  );
+};
+
+export default app;
